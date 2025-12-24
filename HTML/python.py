946c92b15354e92685c0b1aecd1dfa3fd0427c55
dipak-4321 <dipakprince786@gmail.com>
2025-12-24 22:20:11 +0545

    nextpractise

--- HTML/python.py
+++ HTML/python.py
@@ -1,2 +1,15 @@
 list = [2,3,4,5,6,7,8]
 list.reverse()
+print(list)
+str="maama"
+print(str[::-1])
+tup=(3,4,5,)
+
+print(tup[1])
+dic={
+    "name":"Darling",
+    "age":69
+}
+print(dic.items())
+dic={1,2,3,6,3,2,1,6,8}
+print(dic)
