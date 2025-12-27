fee44df949a3971dca77255a979bd7f72802c077
dipak-4321 <dipakprince786@gmail.com>
2025-12-27 22:00:26 +0545

    doing python hm

---
+++ HTML/reverse.py
@@ -0,0 +1,20 @@
+#reverse list
+a = [1,2,3,4,5,6,7,8]
+for i in range(8 ,0,-1):
+    print(i)
+#reverse num
+num = 12345
+rev = 0
+
+for i in range(len(str(num))):
+    rev = rev * 10 + num % 10
+    num //= 10
+    print(i)
+
+print("Reversed number:", rev)
+#factorial
+num=int(input("Enter Number:"))
+fac=1
+for i in range(num,0,-1):
+    fac=fac*i
+print(fac)
